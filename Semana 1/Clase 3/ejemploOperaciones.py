2b5ba8534060796a3296cb466f9544b10acb5213
Jeronimo Orozco <eljeroorozcogomez@gmail.com>
2022-03-03 09:58:06 -0500

    agregar ejemplo de operaciones

---
+++ Semana 1/Clase 3/ejemploOperaciones.py
@@ -0,0 +1,11 @@
+#Paso 1: Preguntarle al usuario el total de la factuta
+totalFactura = int(input("Por favor ingrese el total de su factura: "))
+
+#Paso 2: Pedirle al usario el valor que quiere dar de propina
+valorPropina = int(input("Por favor ingrese el valor que quiere dar de propina: "))
+
+#Paso 3: Hacer el calculo de la propina
+propina = (totalFactura * valorPropina) / 100
+
+#Paso 4: Mostrarle al usario el valor de la propina
+print(f"el total de la propina es de el {propina} $")
