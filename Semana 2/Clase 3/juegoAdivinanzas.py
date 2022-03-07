88e0b4b0ab9d0a4b4fe9f6c955582a6d14bb6d5b
Jeronimo Orozco <eljeroorozcogomez@gmail.com>
2022-03-07 07:52:21 -0500

    corregir error en el juego de adivinanzas

--- Semana 2/Clase 3/juegoAdivinanzas.py
+++ Semana 2/Clase 3/juegoAdivinanzas.py
@@ -5,11 +5,12 @@ numero = 7
 #Le pedimos a la persona que adivine el numero
 
 while numeroPersona != numero:
-    numeroPersona = int(input("Adivina el numero: "))
+    numeroPersona = int(input("Adivina el numero guardado en memoria: "))
     #Evaluamos las opciones
-    if numeroPersona == numero:
-        print("¡Felicidades, has adivinado el numero!")
+    if numeroPersona > numero:
+        print("El numero que has ingresado es mayor que al que debes encontar!")
     elif numeroPersona < numero:
-        print("Fallaste, el numero que debes adivinar es mayor que el que ingresaste")
-    else:
-        print("Fallaste, el numero que debes adivinar es menor que el que ingresaste ")
+        print("El numero que has ingresado es menor que al que debes encontar!")
+
+print("Correcto, has adivinado el numero!")
+        
