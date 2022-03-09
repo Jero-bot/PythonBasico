df7a08d5a81b9066233dcce4863808c6714de4cf
Jeronimo Orozco <eljeroorozcogomez@gmail.com>
2022-03-09 10:34:54 -0500

    cambiar nombre de algunas variables 'ejermploFor - clase 3'

--- Semana 2/Clase 3/ejemploFor.py
+++ Semana 2/Clase 3/ejemploFor.py
@@ -1,8 +1,8 @@
-names = ["Jeronimo", "Daniel", "Maria", "Charles"] # listaCombinada = ["Carola", 123, -67, "Luigi", ["Elemento dentro de una lista"]]
-print(names)
+listOfNames = ["Jeronimo", "Daniel", "Maria", "Charles"] # listaCombinada = ["Carola", 123, -67, "Luigi", ["Elemento dentro de una lista"]]
+print(listOfNames)
 
-for element in names: #Ejemplo de imprimir un elemento de una lista
-    print(element)
+for name in listOfNames: #Ejemplo de imprimir un elemento de una lista
+    print(name)
 
 
 listOfNumbers = [10, 45, -5, 20, 28.5] #Ejemplo de como imprimir los numeros mayores a 15 de una lista
@@ -10,11 +10,11 @@ for number in listOfNumbers:
     if number > 15:
         print(number)
 
-otherList = ["colombia", "uruguay", 355, 89] 
-print(otherList[0])
+countryList = ["colombia", "uruguay", "España", "Alemania"] 
+print(countryList[0])
 
-for index, element in enumerate(otherList): #Ejemplo de como imprimir un elemento y su respectivo indice
-    print(index, element)
+for position, country in enumerate(countryList): #Ejemplo de como imprimir un elemento y su respectivo indice
+    print(position, country)
 
 #Ejemplo de matrix
 matrix = [[1, 26, 3],[4, 8, 6],[7, 2, 45.6]]
